934e8be6114dcc824a3226647e803ed6dad71240
Oluasz <2023211510080@iesp.edu.br>
2025-02-19 21:30:18 -0300

    criando componentes de subtracao, divisao e multiplicacao

--- aula1/src/App.jsx
+++ aula1/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Adicao from './components/Adicao'
+import Subtracao from './components/Subtracao'
 
 const App = () => {
   return (
     <div>
       <Adicao num1={4} num2={6} />
+      <Subtracao num1={4} num2={6} />
     </div>
   )
 }
---
+++ aula1/src/components/Subtracao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Subtracao = (props) => {
+  return (
+    <div>
+        o resultado da subtrção de num 1 e num 2 é: {props.num1 - props.num2}
+    </div>
+  )
+}
+
+export default Subtracao
