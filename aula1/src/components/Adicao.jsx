ea9cf01477a66ae6c8bb208e9784d43c676f1293
Oluasz <2023211510080@iesp.edu.br>
2025-02-19 21:10:00 -0300

    CRIANDO SUBTRACAO E ADICAO

---
+++ aula1/src/components/Adicao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adicao = (props) => {
+  return (
+    <div>
+        o resultado da adição num1 e num2 é: {props.num1 + props.num2}
+    </div>
+  )
+}
+
+export default Adicao
