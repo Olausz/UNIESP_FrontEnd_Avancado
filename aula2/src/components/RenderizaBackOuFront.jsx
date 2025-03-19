a11df4481ebe7f94a20dff0ab187010d7d4bbdd5
Oluasz <2023211510080@iesp.edu.br>
2025-03-19 17:21:21 -0300

    salvando 2

---
+++ aula2/src/components/RenderizaBackOuFront.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const RenderizaBackOuFront = () => {
+
+    function backOuFront (tipoCodigo) {
+        if (tipoCodigo === "Java") {
+            return <h1>Java</h1>
+        } else if (tipoCodigo === "React") {
+            return <b>React</b>
+        } else {
+            return <b>Cloud</b>
+        }
+    }
+  return (
+    <div>
+        {backOuFront("Java")}
+        {backOuFront("React")}
+        {backOuFront("AWS")}
+    </div>
+  )
+}
+
+export default RenderizaBackOuFront
