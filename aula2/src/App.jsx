2a483ef9cf8a13afc99c4ca09853cd346ebf07bd
Oluasz <2023211510080@iesp.edu.br>
2025-03-19 17:53:12 -0300

    salvando final aula2

--- aula2/src/App.jsx
+++ aula2/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import RenderizandoComFuncoes from './components/RenderizandoComFuncoes'
 import RenderizaBackOuFront from './components/RenderizaBackOuFront'
+import TrabalhandoComImagens from './components/TrabalhandoComImagens'
 
 const App = () => {
   return (
     <div>
-      <RenderizaBackOuFront>
+      <TrabalhandoComImagens>
         
-      </RenderizaBackOuFront>
+      </TrabalhandoComImagens>
     </div>
   )
 }
---
+++ aula2/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AnticsImg from '../assets/antics.png'
+
+const TrabalhandoComImagens = () => {
+  return (
+    <div>
+        <img src='interpol.png' alt='Interpol' /><br></br>
+        <img src={AnticsImg} alt='Antics' />
+    </div>
+  )
+}
+
+export default TrabalhandoComImagens
