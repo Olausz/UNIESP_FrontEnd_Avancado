3f7f2aceef4809f578c39787301524c3be550307
Olausz <2023211510080@iesp.edu.br>
2025-04-16 16:05:03 -0300

    Terminando exercicio

--- aula3/src/App.jsx
+++ aula3/src/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-
-import Aluno from './components/Aluno'
+import React from 'react';
+import Aluno from './components/Aluno';
 
 const App = () => {
   return (
     <div>
-      {
-        [ 
-          {nome: "Saulo", email: "saulopenem@gmail.com", curso: "Sistemas para internet"},
-          {nome: "Jaime", email: "drajaime@gmail.com", curso: "Nutrição"},
-          {nome: "Pedro", email: "pedroso1@gmail.com", curso: "Veterinaria"}
-        ].map((aluno) => 
-        <Aluno nome={aluno.nome} email={aluno.email} curso={aluno.curso} />
-        )
-      }
+      {[ 
+        {nome: "Saulo", email: "saulopenem@gmail.com", curso: "Sistemas para internet", media: 8.3},
+        {nome: "Jaime", email: "drajaime@gmail.com", curso: "Nutrição", media: 9.3},
+        {nome: "Pedro", email: "pedroso1@gmail.com", curso: "Veterinaria", media: 6.2}
+      ].map((aluno, index) => 
+        <Aluno 
+          key={index} 
+          nome={aluno.nome} 
+          email={aluno.email} 
+          curso={aluno.curso} 
+          media={aluno.media}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- aula3/src/components/Aluno.jsx
+++ aula3/src/components/Aluno.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const Aluno = (nome, email, curso) => {
+const Aluno = ({ nome, email, curso, media }) => {
+  const status = media >= 7 ? 'APROVADO' : 'REPROVADO';
+  
   return (
     <div>
-        <p>Nome: {nome}</p>
-        <p>Email: {email}</p>
-        <p>Curso: {curso}</p>
-        <hr/>
+      <p>Nome: {nome}</p>
+      <p>Email: {email}</p>
+      <p>Curso: {curso}</p>
+      <p>Média: {media.toFixed(2)}</p>
+      <p>Status: {status}</p>
+      <hr/>
     </div>
-  )
+  );
 }
 
-export default Aluno
+export default Aluno;
